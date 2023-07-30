36bda55d7ace77282a54dcfd381c426bb47b7ac8
fazeel0321 <fazeelbutt00@gmail.com>
2023-07-30 13:26:20 +0500

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class testing extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: setting(),
+      home: customdraw(),
     );
   }
 }
